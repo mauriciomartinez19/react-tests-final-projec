72d59a83cdabf54be53db5ce1f09a8ef24b21531
Mauricio <moromanya@hotmail.com>
2021-12-27 13:09:49 -0300

    Add React Router Dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-
-import './App.css';
+import React from 'react';
 // react router
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 //Navbar
-import TopNavbar from '../Navbar/TopNavbar';
+import TopNavbar from './Navbar/TopNavbar';
+
+//Pages
+
+import Home from './Home/Home';
+import MenuPage from './Menu'
+import ToursPage from './Tours/ToursPage';
+
 
 function App() {
-  return (
+  return (<Router>
     <TopNavbar />
+    <Route exact path='/'>
+      <Home />
+    </Route>
+    <Route path='/menu'>
+      <MenuPage />
+    </Route>
+    <Route path='/tours'>
+      <ToursPage />
+    </Route>
+  </Router>
   );
 }
 
