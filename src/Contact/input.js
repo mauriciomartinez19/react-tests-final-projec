89cd07dfa8d9809f0e4450ab07879997dacae4a5
Mauricio <moromanya@hotmail.com>
2021-12-28 11:31:38 -0300

    Add css style to contact page

--- src/Contact/input.js
+++ src/Contact/input.js
@@ -1,10 +1,14 @@
+
+
 const Input = () => {
     return <section className="input-section">
         <h1 className="input-title">Get in Touch!</h1>
-        <input placeholder="name" />
-        <input placeholder="email" />
-        <textarea placeholder="message" />
-        <button type="submit">Send</button>
+        <form className="formulario">
+            <input placeholder="name" />
+            <input placeholder="email" />
+            <textarea placeholder="message" />
+            <button type="submit" className="submit-btn">Send</button>
+        </form>
     </section>
 }
 
