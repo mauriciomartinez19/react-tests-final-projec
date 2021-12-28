d0a66025e5cbbfb9dbd7ab3d5c1e403271cd6ce4
Mauricio <moromanya@hotmail.com>
2021-12-28 12:04:34 -0300

    Add functions to input form

--- src/Contact/input.js
+++ src/Contact/input.js
@@ -1,12 +1,40 @@
-
+import { useState } from "react"
 
 const Input = () => {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [message, setMessage] = useState('')
+    const [sendMes, setSendMes] = useState({})
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const data = { id: new Date().getTime().toString(), name, email, message }
+        setSendMes(data)
+        console.log(data)
+        setName('')
+        setEmail('')
+        setMessage('')
+    }
     return <section className="input-section">
         <h1 className="input-title">Get in Touch!</h1>
-        <form className="formulario">
-            <input placeholder="name" />
-            <input placeholder="email" />
-            <textarea placeholder="message" />
+        <form className="formulario" onSubmit={handleSubmit}>
+            <input
+                type='text'
+                value={name}
+                placeholder="name"
+                onChange={(e) => setName(e.target.value)}
+                required />
+            <input
+                type='email'
+                value={email}
+                placeholder="email"
+                onChange={(e) => setEmail(e.target.value)}
+                required />
+            <textarea
+                value={message}
+                placeholder="message"
+                onChange={(e) => setMessage(e.target.value)}
+                required />
             <button type="submit" className="submit-btn">Send</button>
         </form>
     </section>
