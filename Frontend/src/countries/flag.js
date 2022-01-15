4c4b655f5d44df3d79e008777c7a1e2c8ebc4011
Mauricio <moromanya@hotmail.com>
2022-01-15 16:18:14 -0300

    Add flip style to flags on country page

--- Frontend/src/countries/flag.js
+++ Frontend/src/countries/flag.js
@@ -9,72 +9,76 @@ const Flag = ({ name, firstid, secid, uncode, population, price, School, Co2, PB
 
     if (showFlag) {
         return (<section className="country">
-            <div className="flag-box" onClick={() => setShowFlag(!showFlag)}>
-                <img src={`${FLAG_BASE_URI}${firstid}`} alt={name + ': flag'} className="flag" />
-            </div>
-            <div className="name-box">
-                <h1 className="name">{name}</h1>
-            </div>
-            <div className="botton-zone">
-                <div className="text-zone">
-                    <div className="sub-title">
-                        <h5 className="tag">ISO Alpha-2: </h5>
-                        <p>{firstid}</p>
-                    </div>
-                    <div className="sub-title">
-                        <h5 className="tag">ISO Alpha-3:  </h5>
-                        <p>{secid}</p>
-                    </div>
-                    <div className="sub-title">
-                        <h5 className="tag">UN code: </h5>
-                        <p>{uncode}</p>
-                    </div>
+            <div className="flip">
+                <div className="flag-box" onClick={() => setShowFlag(!showFlag)}>
+                    <img src={`${FLAG_BASE_URI}${firstid}`} alt={name + ': flag'} className="flag" />
                 </div>
-                <button className="cart-btn" value={uncode} onClick={(e) => wishlistContext.addToWishlist(e)}>Add to wishlist</button>
+                <div className="name-box">
+                    <h1 className="name">{name}</h1>
+                </div>
+                <div className="botton-zone">
+                    <div className="text-zone">
+                        <div className="sub-title">
+                            <h5 className="tag">ISO Alpha-2: </h5>
+                            <p>{firstid}</p>
+                        </div>
+                        <div className="sub-title">
+                            <h5 className="tag">ISO Alpha-3:  </h5>
+                            <p>{secid}</p>
+                        </div>
+                        <div className="sub-title">
+                            <h5 className="tag">UN code: </h5>
+                            <p>{uncode}</p>
+                        </div>
+                    </div>
+                    <button className="cart-btn" value={uncode} onClick={(e) => wishlistContext.addToWishlist(e)}>Add to wishlist</button>
+                </div>
             </div>
         </section>)
     }
     return (
-        <section className="country">
-            <div className="flag-box-reverse" onClick={() => setShowFlag(!showFlag)}>
-                <div className="price-box">
-                    <h1 className="name-reverse">{name}</h1>
-                    <div className="price-zone">
-                        <h5 className="price">Price: </h5>
-                        <p className="price-number">{'$ ' + price}</p>
+        <section className="country-reverse">
+            <div className="flip-reverse">
+                <div className="flag-box-reverse" onClick={() => setShowFlag(!showFlag)}>
+                    <div className="price-box">
+                        <h1 className="name-reverse">{name}</h1>
+                        <div className="price-zone">
+                            <h5 className="price">Price: </h5>
+                            <p className="price-number">{'$ ' + price}</p>
+                        </div>
                     </div>
+                    <img src={`${FLAG_BASE_URI}${firstid}`} alt={name + ': flag'} className="flag-reverse" />
                 </div>
-                <img src={`${FLAG_BASE_URI}${firstid}`} alt={name + ': flag'} className="flag-reverse" />
-            </div>
-            <div className="botton">
-                <div className="text-zone-reverse">
-                    <div className="sub-title">
-                        <h5 className="tag">Population: </h5>
-                        <p>{population}</p>
+                <div className="botton">
+                    <div className="text-zone-reverse">
+                        <div className="sub-title">
+                            <h5 className="tag">Population: </h5>
+                            <p>{population}</p>
+                        </div>
+                        <div className="sub-title">
+                            <h5 className="tag">School enrollament:  </h5>
+                            <p>{School}</p>
+                        </div>
+                        <div className="sub-title">
+                            <h5 className="tag">Co2 contamination: </h5>
+                            <p>{Co2}</p>
+                        </div>
+                        <div className="sub-title">
+                            <h5 className="tag">PBI: </h5>
+                            <p>{'$ ' + PBI}</p>
+                        </div>
+                        <div className="sub-title">
+                            <h5 className="tag">Life expectation: </h5>
+                            <p>{lifeexp + ' years'}</p>
+                        </div>
+                        <div className="sub-title">
+                            <h5 className="tag">Covid cases: </h5>
+                            <p>{Covid}</p>
+                        </div>
                     </div>
-                    <div className="sub-title">
-                        <h5 className="tag">School enrollament:  </h5>
-                        <p>{School}</p>
+                    <div className="btn-div">
+                        <button onClick={() => setShowFlag(!showFlag)}>Back to flag</button>
                     </div>
-                    <div className="sub-title">
-                        <h5 className="tag">Co2 contamination: </h5>
-                        <p>{Co2}</p>
-                    </div>
-                    <div className="sub-title">
-                        <h5 className="tag">PBI: </h5>
-                        <p>{'$ ' + PBI}</p>
-                    </div>
-                    <div className="sub-title">
-                        <h5 className="tag">Life expectation: </h5>
-                        <p>{lifeexp + ' years'}</p>
-                    </div>
-                    <div className="sub-title">
-                        <h5 className="tag">Covid cases: </h5>
-                        <p>{Covid}</p>
-                    </div>
-                </div>
-                <div className="btn-div">
-                    <button onClick={() => setShowFlag(!showFlag)}>Back to flag</button>
                 </div>
             </div>
         </section>
