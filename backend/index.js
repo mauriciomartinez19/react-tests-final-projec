076ab661942229f84315715cac676304badf8cfc
Mauricio <moromanya@hotmail.com>
2022-01-17 11:37:20 -0300

    Move pages routes to folder pages

--- backend/index.js
+++ backend/index.js
@@ -6,10 +6,10 @@ app.use(cors())
 
 app.use(express.json())
 
-const tours = require('./tours/tour-routes')
-const reviews = require('./reviews/reviews-routes')
-const contacts = require('./admin-contact/admin-contact-routes')
-const countries = require('./countries/countries-routes')
+const tours = require('./pages/tours/tour-routes')
+const reviews = require('./pages/reviews/reviews-routes')
+const contacts = require('./pages/admin-contact/admin-contact-routes')
+const countries = require('./pages/countries/countries-routes')
 
 app.use('/api/tours-data', tours)
 app.use('/api/reviews-data', reviews)
