991132558131e831585dcc37e4033de94b343e76
Mauricio <moromanya@hotmail.com>
2022-01-17 17:36:17 -0300

    Change wishlist schema

--- backend/database/models/countries/wishlist.js
+++ backend/database/models/countries/wishlist.js
@@ -4,16 +4,8 @@ const { Schema } = mongoose;
 const WishlistSchema = new Schema({
     name: { type: String, required: true },
     firstid: { type: String, required: true },
-    secid: { type: String, required: true },
-    uncode: { type: Number, required: true },
-    population: { type: Number, required: true },
     price: { type: Number, required: true },
-    School: { type: Number, required: true },
-    Co2: { type: Number, required: true },
-    PBI: { type: Number, required: true },
-    lifeexp: { type: Number, required: true },
-    Covid: { type: Number, required: true },
-    quantity: { type: Number, required: false }
+    quantity: { type: Number, required: false },
 });
 
 module.exports = mongoose.model('Wishlist', WishlistSchema);
