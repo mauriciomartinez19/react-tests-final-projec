f208c1a55a77149bb071a6f2a07c17fa47bd17fe
Mauricio <moromanya@hotmail.com>
2022-01-17 11:10:02 -0300

    Change style of Card section

--- Frontend/src/countries/card.js
+++ Frontend/src/countries/card.js
@@ -11,18 +11,22 @@ const Card = ({ showFlag, children }) => {
 }
 
 const Country = styled.section`
-    border: solid black 2px;
-    border-radius:10px;
-    width: 250px;
+    width: 252px;
     background-color: ${props => props.showFlag ? 'rgb(255,255,255,0.30)' : 'rgb(209, 228, 245,0.30)'};
     height: 400px;
-    transition: 1s;
-    transform-style: preserve-3d;
+    position: relative;
     ${props => !props.showFlag && 'transform: rotateY(180deg);'}
-`;
+`
 
 const Flip = styled.div`
-    backface-visibility: hidden;
+    border: solid black 2px;
+    border-radius:10px;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    transition: 1s;
+    transform-style: preserve-3d;
+    background-color: rgb(255,255,255,0.30);
     ${props => !props.showFlag && 'transform: rotateY(180deg);'}
 `
 
