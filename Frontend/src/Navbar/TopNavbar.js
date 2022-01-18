32ff9752052b49da323c1c7da05ab53b4234a5fb
Mauricio <moromanya@hotmail.com>
2022-01-18 11:49:31 -0300

    Change admin contact name to testimonial

--- Frontend/src/Navbar/TopNavbar.js
+++ Frontend/src/Navbar/TopNavbar.js
@@ -30,7 +30,7 @@ const TopNavbar = () => {
       <ul className='list'>
         <Link to='/'>Home</Link>
         <Link to='/contact'>Contact</Link>
-        <Link to='/admin-contacts'>Admin contact</Link>
+        <Link to='/admin-contacts'>Testimonial</Link>
       </ul>
     </nav>
   }
