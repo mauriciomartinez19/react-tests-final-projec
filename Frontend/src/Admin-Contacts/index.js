b474d588dfd0913fcac5ac692a9c47cb546410d5
Mauricio <moromanya@hotmail.com>
2022-01-18 11:50:00 -0300

    Add bottom separator in Admin contact page

--- Frontend/src/Admin-Contacts/index.js
+++ Frontend/src/Admin-Contacts/index.js
@@ -5,6 +5,7 @@ const AdminContact = () => {
     return <div className="page-admin-contact">
         <div className="nav-sep-admin-contact" />
         <Contacts />
+        <div className="nav-sep-admin-contact" />
     </div>
 }
 
