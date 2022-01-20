ea009fe3ae8d4c3ee046581d416acd6acd58d9e0
Mauricio <moromanya@hotmail.com>
2022-01-20 14:07:57 -0300

    Actaulizate the UserSchema

--- backend/database/models/login/user.js
+++ backend/database/models/login/user.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose')
-const { schema } = mongoose
+const { Schema } = mongoose
 
 const UserSchema = new Schema({
     userName: { type: String, required: true, index: { unique: true } },
+    email: { type: String, required: true, index: { unique: true } },
+    age: { type: Number, required: true },
     passwordHash: { type: String, required: true }
 })
 
-module.exports = mongoose.mosel('User', UserSchema)
+module.exports = mongoose.model('User', UserSchema)
