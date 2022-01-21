c1650660bb9226927319a3c4fd1304bf90feac28
Mauricio <moromanya@hotmail.com>
2022-01-21 09:42:14 -0300

    Set the userName in the navbar when is login

--- Frontend/src/Navbar/TopNavbar.js
+++ Frontend/src/Navbar/TopNavbar.js
@@ -2,14 +2,33 @@ import './TopNavbar.css'
 import restlogo from './restlogo.png'
 import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom';
+import jwt_decode from "jwt-decode";
 
 const TopNavbar = () => {
 
   const [scrollState, setScrollState] = useState("top")
+  const [userName, setUserName] = useState(false)
+  const [dropdown, setDropdown] = useState(false)
+
+  const logout = () => {
+    localStorage.removeItem('token')
+    setUserName(false)
+    console.log(userName)
+  }
 
   useEffect(() => {
+
+    const token = localStorage.getItem('token')
+
+    if (token) {
+      let decoded = jwt_decode(token)
+      const name = decoded.name
+      setUserName(name)
+      console.log(decoded)
+    }
+
     const listener = document.addEventListener("scroll", e => {
-      var scrolled = document.scrollingElement.scrollTop
+      let scrolled = document.scrollingElement.scrollTop
       if (scrolled >= 120) {
         if (scrollState !== "amir") {
           setScrollState("amir")
@@ -23,7 +42,7 @@ const TopNavbar = () => {
     return () => {
       document.removeEventListener("scroll", listener)
     }
-  }, [scrollState])
+  }, [scrollState, userName])
   if (scrollState === "top") {
     return <nav className="navbar-box">
       <img src={restlogo} className='restlogo' />
@@ -31,6 +50,16 @@ const TopNavbar = () => {
         <Link to='/'>Home</Link>
         <Link to='/contact'>Contact</Link>
         <Link to='/admin-contacts'>Testimonial</Link>
+        {userName ? <div>
+          <button className="dropbtn" onClick={() => setDropdown(!dropdown)}>{userName}</button>
+          {dropdown ? <div className="dropdown-content">
+            <a onClick={() => logout()}>Logout</a>
+          </div>
+            : <></>
+          }
+
+        </div>
+          : <Link to='/login'>Sign Up</Link>}
       </ul>
     </nav>
   }
